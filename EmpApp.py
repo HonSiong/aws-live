fbab8277db0c5a69aba0bb2ea07b91dfd2112385
可以可以不要紧 <chowhs-wm19@student.tarc.edu.my>
2022-09-23 11:58:37 +0800

    EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -146,7 +146,7 @@ def editprofile(empid):
 
 
     ######################Delete Employee Page#############################################
-@app.route("/deleteEmployee", methods=['GET','POST'])
+@app.route("/deleteEmployee", methods=['GET'])
 def deleteEmployee():
     return render_template('deleteEmployee.html')
 
