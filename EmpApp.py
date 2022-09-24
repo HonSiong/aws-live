6aada19b93caa54365561849ab09a778eea8f9e0
可以可以不要紧 <chowhs-wm19@student.tarc.edu.my>
2022-09-24 14:12:19 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -133,13 +133,48 @@ def editempdb():
     basicSalary = request.form['basicSalary']
     status = request.form['status']
     date_of_birth = request.form['date_of_birth']
+    emp_image_file = request.files['emp_image_file']
 
-    updatesql = "UPDATE `employee` SET `emp_id` = %s, `first_name` = %s, `last_name` = %s, `email` = %s, `phoneNum` = %s, `pri_skill` = %s, `address` = %s, `position` = %s, `department` = %s, `basicSalary` = %s, `status` = %s, `date_of_birth` = %s WHERE `emp_id` = %s"
+    updatesql = "UPDATE `employee` SET `emp_id` = %s, `first_name` = %s, `last_name` = %s, `email` = %s, `phoneNum` = %s, `pri_skill` = %s, `address` = %s, `image_path = %s`, `position` = %s, `department` = %s, `basicSalary` = %s, `status` = %s, `date_of_birth` = %s WHERE `emp_id` = %s"
     cursor = db_conn.cursor()
-    cursor.execute(updatesql,(emp_id, first_name, last_name, email, phoneNum, pri_skill, address, position, department, basicSalary, status, date_of_birth, emp_id))
-    db_conn.commit()
     
-    return render_template('updateOutput.html', empid=emp_id)
+    if emp_image_file.filename == "":
+            return "Please select a file"
+
+        try:
+
+            
+            emp_name = "" + first_name + " " + last_name
+            # Uplaod image file in S3 #
+            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+            s3 = boto3.resource('s3')
+
+            try:
+                print("Data inserted in MySQL RDS... uploading image to S3...")
+                s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+                s3_location = (bucket_location['LocationConstraint'])
+
+                if s3_location is None:
+                    s3_location = ''
+                else:
+                    s3_location = '-' + s3_location
+
+                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                    s3_location,
+                    custombucket,
+                    emp_image_file_name_in_s3)
+
+            except Exception as e:
+                return str(e)
+            cursor.execute(updatesql,(emp_id, first_name, last_name, email, phoneNum, pri_skill, address, object_url, position, department, basicSalary, status, date_of_birth, emp_id))
+            db_conn.commit()
+
+        finally:
+            cursor.close()
+
+        print("all modification done...")
+        return render_template('updateOutput.html', empid=emp_id)
 
 ######################Delete Employee Page#############################################
 
