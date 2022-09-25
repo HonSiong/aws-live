dcb364027c242ef7e2fbbefa0f89c2215832c4da
可以可以不要紧 <chowhs-wm19@student.tarc.edu.my>
2022-09-25 23:37:06 +0800

    e

--- EmpApp.py
+++ EmpApp.py
@@ -71,9 +71,6 @@ def viewemp():
         return render_template('employee.html', emps=emps, emps2=emps2, emps3=emps3)
 
 #######################Profile Page#########################################################
-@app.route("/", methods=['GET'])
-def pro():
-    return render_template('profile.html')
 
 @app.route("/profile/<empid>")
 def profile(empid):
