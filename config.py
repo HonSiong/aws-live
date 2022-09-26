1ac73da8ae16c9d265a8eb09dd7f62df2cd2e35e
可以可以不要紧 <77868881+HonSiong@users.noreply.github.com>
2022-09-26 23:53:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import pymysql
+
 customhost = "employee.cdt8o0iosgfy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
 custombucket = "chowhonsiong-bucket"
 customregion = "us-east-1"
+cursorType = pymysql.cursors.DictCursor
+maxConnections = 8
